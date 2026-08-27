148268d6d03c35cc5962012cc2ce8d212e0ca7b7
EngrityGit <prathik.jeyakumar@engrity.com>
2026-08-27 11:07:22 -0600

    fixes 5

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'; // Added CookieOptions type
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
@@ -12,11 +12,13 @@ export async function middleware(request: NextRequest) {
         get(name: string) {
           return request.cookies.get(name)?.value;
         },
-        set(name: string, value: string, options) {
+        // Added : CookieOptions type here
+        set(name: string, value: string, options: CookieOptions) {
           response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value, ...options });
         },
-        remove(name: string, options) {
+        // Added : CookieOptions type here
+        remove(name: string, options: CookieOptions) {
           response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value: '', ...options });
         },
